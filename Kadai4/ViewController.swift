b3de652341b084853af6e3080e221d9b75a84b0a
Hiroatsu <hiroatsu.7190@gmail.com>
2021-09-14 16:24:17 +0900

    Initial Commit

---
+++ Kadai4/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Kadai4
+//
+//  Created by Hiroatsu on 2021/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var countLabel: UILabel!
+    private var count = 0
+
+    @IBAction private func plusOneButton(_ sender: UIButton) {
+        count += 1
+        countLabel.text = String(count)
+    }
+
+    @IBAction private func clearButton(_ sender: UIButton) {
+        count = 0
+        countLabel.text = String(count)
+    }
+}
